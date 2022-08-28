afa53b2cf93fcba5aafdb176545c1abbf9ed0000
yonisokolov <sokolov1@mail.tau.ac.il>
2022-08-28 15:36:03 +0300

    step 1 completed - not tested. see that the matrix W created is mutable.

--- src/calculate_w.c
+++ src/calculate_w.c
@@ -1,23 +1,33 @@
+#include "datastructures.h"
+#include "math.h"
 //step1 - create fun called "distanceL2"
 //step2 - use math.h to calculate exp(-...)
 //step3 - use shais matrix class and create the new W matrix
 
-typedef MATRIX{ 
-    data
-    num cols
-    num rows
-}
-
 double distance_L2(double *v1, double *v2, unsigned int len){
     double result = 0;
-    for (int a = 0; a < len ; a += 1 ){
+    for (int a = 0; a < len ; a++ ){
         result += (v1[a] - v2[a])*(v1[a] - v2[a]);
     }
     result = sqrt(result);
     return result;
 }
 
-MATRIX create_W(MATRIX V)
+MATRIX *create_W(MATRIX *V)
 {
-    
+    MATRIX *w;
+    int n = V->num_cols;
+    w->num_cols = n;
+    w->num_rows = n;
+    for(int i=0; i<n; i++){
+        for(int j=0;j<n;j++){
+            if (i==j){
+                get_m(w,i,j) = 0;
+            }
+            else{
+                    get_m(w,i,j) = exp(-distance_L2(V->data[i],V->data[j],V->num_rows)/2);
+            }
+        }
+    }
+    return w;
 }
