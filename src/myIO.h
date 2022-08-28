8f581dbad9f906653bcd1f8b20916835cc10625f
shai-mermelstein <shai.mermelstein@gmail.com>
2022-08-28 13:33:43 +0300

    added documentation for read vectors

--- src/myIO.h
+++ src/myIO.h
@@ -3,6 +3,12 @@
 
 #include "datastructures.h"
 
+/**
+ * Reads vectors form file with given file name.
+ * Each row is treated as a vector.
+ * Returns a matrix, with a column for each vector.
+ * Assumes correct formatting of file.
+ */
 const MATRIX read_vectors_from_file(const char *file_name);
 
 #endif //__myIO__
