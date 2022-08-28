87b04f515768c4ea5801777d3eea21ca08f4d1c6
shai-mermelstein <shai.mermelstein@gmail.com>
2022-08-28 14:27:15 +0300

    wrote read_vectors_from_file

--- src/myIO.c
+++ src/myIO.c
@@ -3,6 +3,33 @@
 #include <stdio.h>
 
 MATRIX *read_vectors_from_file(const char *file_name) {
-    FILE *f = fopen(file_name, "r");
+    FILE *file = fopen(file_name, "r");
+    MATRIX *res;
+    unsigned vec_dim = 1, num_vecs = 1;
+    if (!file) return NULL;
 
+    /* find vec_dim and num_vecs */
+    {
+        int c;
+        for(c = getc(file); c != '\n'; c = getc(file))
+            if (c == ',') vec_dim++;
+        for(c = getc(file); c != EOF; c = getc(file))
+            if (c == '\n') num_vecs++;
+        fseek(file, 0, SEEK_SET);
+    }
+
+    res = alloc_matrix(vec_dim, num_vecs);
+    if (!res) return NULL;
+
+    {
+        unsigned i, j;
+        for (j = 0; j < num_vecs; j++)
+            for (i = 0; i < vec_dim; i++) {
+                fscanf(file, "%lf", &get_m(res, i, j));
+                getc(file);
+            }
+    }
+    
+    fclose(file);
+    return res;
 }
--- src/myIO.h
+++ src/myIO.h
@@ -6,7 +6,7 @@
 /**
  * Reads vectors form file with given file name.
  * Each row is treated as a vector.
- * Returns a matrix, with a column for each vector.
+ * Returns a matrix, with a column for each vector, or NULL on failure.
  * Assumes correct formatting of file and of file_name.
  */
 MATRIX *read_vectors_from_file(const char *file_name);
