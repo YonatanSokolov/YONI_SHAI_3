61b6ef9bd2318eed9b7c09d86d46bddec46dfe12
shai-mermelstein <shai.mermelstein@gmail.com>
2022-08-30 18:53:34 +0300

    finished eigengap

--- src/eigengap.c
+++ src/eigengap.c
@@ -4,80 +4,79 @@
 #include <stdbool.h>
 #include <string.h>
 
-#include <stdio.h>
-
 typedef struct {
     double val;
     unsigned pos;
-} VAL_W_POS;
+} KEY;
 
 /**
- * Quick-sort a VAL_W_POS array.
+ * Quick-sort a key array.
  */
-static void sort_VAL_W_POS_rec(VAL_W_POS *arr, unsigned length) {
-    #define swap(i, j)  do {       \
-        VAL_W_POS temp = arr[i];   \
-        arr[i] = arr[j];           \
-        arr[j] = temp;             \
+static void sort_rec(KEY *arr, unsigned length) {
+    #define cmp(i, j, f, c)   (arr[i].f c arr[j].f)
+    #define greater(i, j)   \
+        (cmp(i, j, val, ==) * cmp(i, j, pos, <) + cmp(i, j, val, >))
+    #define swap(i, j)  do {    \
+        KEY temp = arr[i];      \
+        arr[i] = arr[j];        \
+        arr[j] = temp;          \
         } while (0)   
 
-    #define larger(i, j)    ((arr[i].val > arr[j].val) ? true : (arr[i].pos < arr[j].pos))
-
     unsigned start = 0, end = --length;
-    if (end - start == 1) {
-        if (larger(end, start)) swap(end, start);
+    if (end - start == 1 && greater(end, start)) {
+        swap(end, start);
+        return;
     }
     else while (start < end) {
-        if (larger(length, start)) {
+        if (greater(length, start)) {
             end--;
             swap(start, end);
         }
         else start++;
     }
     swap(end, length);
-    if (end) sort_VAL_W_POS_rec(arr, end);
-    if (length - end) sort_VAL_W_POS_rec(arr + end + 1, length - end);
+    if (end) sort_rec(arr, end);
+    if (length - end) sort_rec(arr + end + 1, length - end);
+
+    #undef cmp
+    #undef greater
+    #undef swap
 }
 
 /**
  * Sorts matrix and vector by vector's elements inplace.
  */
-static int sort_by_vector(MAT_AND_VEC *U) {
+static int sort_inplace(MAT_AND_VEC *U) {
     #define at(t, i)   v_at(U->t, i)
 
     unsigned length = len(U->vector), i;
-    VAL_W_POS *vps = (VAL_W_POS *) malloc(length * sizeof(VAL_W_POS));
+    KEY *vps = (KEY *) malloc(length * sizeof(KEY));
     double **new_data = (double **) malloc(length * sizeof(double *));
     if (!vps || !new_data) return 1;
 
     for (i = 0; i < length; i++) {
-        VAL_W_POS temp;
+        KEY temp;
         temp.val = at(vector, i);
         temp.pos = i;
         vps[i] = temp;
     }
-    sort_VAL_W_POS_rec(vps, length);
+    sort_rec(vps, length);
     for (i = 0; i < length; i++)
         new_data[i] = at(matrix, vps[i].pos);
     U->matrix->data = new_data;
-    for (i = 0; i < length; i++) {
+    for (i = 0; i < length; i++)
         at(vector, i) = vps[i].val;
-        printf("%f\n", vps[i].val);
-    }
     return 0;
+
+    #undef at
 }
 
 MATRIX *reduced_vectors(MAT_AND_VEC U) {
-    unsigned length = U.vector->length, k, i;
+    unsigned length = U.vector->length, k, i, j;
     double max = -1;
+    MATRIX *res;
 
-    for (i = 0; i < length; i++) printf("%f, ", v_at(U.vector, i));
-    printf("\n");
-
-    if (sort_by_vector(&U)) return NULL;
-
-    for (i = 0; i < length; i++) printf("%f, ", v_at(U.vector, i));
-    printf("\n");
+    if (sort_inplace(&U)) return NULL;
 
     for (i = 0; i < length / 2; i++) {
         double delta = v_at(U.vector, i) - v_at(U.vector, i + 1);
@@ -87,10 +86,9 @@ MATRIX *reduced_vectors(MAT_AND_VEC U) {
         }
     }
 
-    {
-        MATRIX *res = alloc_matrix(U.matrix->num_rows, k);
-        if (!res) return NULL;
-        memcpy(res->data, U.matrix->data, k * sizeof(double *));
-        return res;
-    }
+    res = alloc_matrix(U.matrix->num_rows, ++k);
+    if (!res) return NULL;
+    for (j = 0; j < k; j ++) for (i = 0; i < U.matrix->num_rows; i++)
+        m_at(res, i, j) = m_at(U.matrix, i, j);
+    return res;
 }
