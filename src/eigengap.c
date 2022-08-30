bdacaa76dd6a8635499db4447d17020c53a93617
shai-mermelstein <shai.mermelstein@gmail.com>
2022-08-30 16:25:24 +0300

    started work on

--- src/eigengap.c
+++ src/eigengap.c
@@ -2,6 +2,9 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
+#include <stdio.h>
 
 typedef struct {
     double val;
@@ -18,30 +21,28 @@ static void sort_VAL_W_POS_rec(VAL_W_POS *arr, unsigned length) {
         arr[j] = temp;             \
         } while (0)   
 
-    #define larger(i, j)    ((arr[i].val > arr[j].val) ?: (arr[i].pos > arr[j].pos))
+    #define larger(i, j)    ((arr[i].val > arr[j].val) ? true : (arr[i].pos < arr[j].pos))
 
-    unsigned start = 0, end = length;
-    if (end - start == 2) {
-        if (larger(end - 1, start)) swap(end - 1, start);
+    unsigned start = 0, end = --length;
+    if (end - start == 1) {
+        if (larger(end, start)) swap(end, start);
     }
-    else if (end - start > 2) {
-        while (start + 1 < end) {
-            if (larger(length - 1, start)) {
-                end--;
-                swap(start, end);
-            }
-            else start++;
+    else while (start < end) {
+        if (larger(length, start)) {
+            end--;
+            swap(start, end);
         }
-        swap(end, length - 1);
-        sort_VAL_W_POS_rec(arr, end);
-        sort_VAL_W_POS_rec(arr + (end+1)*sizeof(VAL_W_POS), length - end - 1);
+        else start++;
     }
+    swap(end, length);
+    if (end) sort_VAL_W_POS_rec(arr, end);
+    if (length - end) sort_VAL_W_POS_rec(arr + end + 1, length - end);
 }
 
 /**
  * Sorts matrix and vector by vector's elements inplace.
  */
-static int sort_by_vector(MAT_AND_VEC *U, unsigned start, unsigned end) {
+static int sort_by_vector(MAT_AND_VEC *U) {
     #define at(t, i)   v_at(U->t, i)
 
     unsigned length = len(U->vector), i;
@@ -49,21 +50,47 @@ static int sort_by_vector(MAT_AND_VEC *U, unsigned start, unsigned end) {
     double **new_data = (double **) malloc(length * sizeof(double *));
     if (!vps || !new_data) return 1;
 
-    for (i = 0; i < length; i++) 
-        vps[i] = (VAL_W_POS) {.val = at(vector, i), .pos = i};
+    for (i = 0; i < length; i++) {
+        VAL_W_POS temp;
+        temp.val = at(vector, i);
+        temp.pos = i;
+        vps[i] = temp;
+    }
     sort_VAL_W_POS_rec(vps, length);
     for (i = 0; i < length; i++)
         new_data[i] = at(matrix, vps[i].pos);
     U->matrix->data = new_data;
-    for (i = 0; i < length; i++)
+    for (i = 0; i < length; i++) {
         at(vector, i) = vps[i].val;
+        printf("%f\n", vps[i].val);
+    }
     return 0;
 }
 
 MATRIX *reduced_vectors(MAT_AND_VEC U) {
-    unsigned length = U.vector->length, k = 0;
+    unsigned length = U.vector->length, k, i;
     double max = -1;
-    if (sort_by_vector_inplace(0, length)) return NULL;
 
+    for (i = 0; i < length; i++) printf("%f, ", v_at(U.vector, i));
+    printf("\n");
 
+    if (sort_by_vector(&U)) return NULL;
+
+    for (i = 0; i < length; i++) printf("%f, ", v_at(U.vector, i));
+    printf("\n");
+
+    for (i = 0; i < length / 2; i++) {
+        double delta = v_at(U.vector, i) - v_at(U.vector, i + 1);
+        if (delta > max) {
+            max = delta;
+            k = i;
+        }
+    }
+
+    {
+        MATRIX *res = alloc_matrix(U.matrix->num_rows, k);
+        if (!res) return NULL;
+        memcpy(res->data, U.matrix->data, k * sizeof(double *));
+        return res;
+    }
 }
--- test/eigengap.c
+++ test/eigengap.c
@@ -6,10 +6,13 @@
 void test_reduced_vectors() {
     MATRIX *M = read_vectors_from_file("test/eigengap_samp1.txt");
     VECTOR *V = & (VECTOR) {.data = v_at(M, 0), .length = 7};
+    for (int i = 0; i < 7; i++) printf("%lf, ", v_at(V, i));
+    printf("\n");
     MAT_AND_VEC input = {M, V};
     MATRIX *result = reduced_vectors(input);
 
     assert(result);
+    printf("num_cols = %u\n", result->num_cols);
     assert(result->num_cols == 2);
     assert(m_at(result, 0, 0) == 9);
     assert(m_at(result, 0, 1) == 9);
