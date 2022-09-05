e58a770bd1aa8638f5ecf16d20d17e254360f6d4
shai-mermelstein <shai.mermelstein@gmail.com>
2022-09-05 18:26:52 +0300

    edited test

--- test/shaijacobi.c
+++ test/shaijacobi.c
@@ -1,22 +1,39 @@
 #include "../src/myIO.h"
 #include "../src/shaijacobi.h"
 #include <assert.h>
+#include <math.h>
 #include <stdio.h>
+
+#define espsilon 0.0001
 
 void test_eigenvalues_eigenvectors() {
     MATRIX A = read_vectors_from_file("test/shaijaconi_samp1.txt");
-
-    printf("S = \n");
-    print_matrix(A);
-
+    // printf("S = \n");
+    // print_matrix(A);
     MAT_AND_VEC res = eigenvalues_eigenvectors(A);
-    printf("res = \n");
-    print_matrix_and_vector(res);
+    MAT_AND_VEC prob_res = {
+        .vector = {3, (double []) {1.6099,0.2357,1.1545}},
+        .matrix = {3, 3, 
+            (double []) {0.7283,0.6565,-0.1964,-0.6712,0.6256,-0.3976,-0.1381,0.4214,0.8963}
+        }
+    };
+    // printf("res = \n");
+    // print_matrix_and_vector(res);
+    
+    assert(!is_null(res.matrix));
+    assert(!is_null(res.vector));
+    assert(res.matrix.num_cols == 3);
+    assert(res.matrix.num_rows == 3);
+    assert(res.vector.length == 3);
+    for (int i = 0; i < 3; i++)
+        assert(fabs(v_at(res.vector, i) - v_at(prob_res.vector, i)) < espsilon);
+    for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++)
+        assert(fabs(m_at(res.matrix, i, j) - m_at(prob_res.matrix, i, j)) < espsilon);
 
     free_matrix(A);
     free_matrix(res.matrix);
     free_vector(res.vector);
-    printf("test eigenvalues_eigenvectors DONE\n");
+    printf("~test~ eigenvalues_eigenvectors DONE\n");
 
     /* should print:
             S = 
