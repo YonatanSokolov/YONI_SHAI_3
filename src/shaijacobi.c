9c725f80a1b8506296d7e061b173a0d9c4b8e9a7
shai-mermelstein <shai.mermelstein@gmail.com>
2022-09-05 17:39:42 +0300

    miner edits

--- src/shaijacobi.c
+++ src/shaijacobi.c
@@ -1,8 +1,9 @@
 #include "shaijacobi.h"
-#include <float.h>
 #include <stdlib.h>
 #include <math.h>
 
+#define maxIter     100
+#define epsilon     0.000005
 #define square(a)   ((a)*(a))
 
 static MATRIX identity_matrix(unsigned n) {
@@ -15,20 +16,20 @@ static MATRIX identity_matrix(unsigned n) {
 }
 
 static void find_largest_offdiagonal_and_sum_upper
-                    (MATRIX S, unsigned *resI, unsigned *resJ, double *sum) {
+                    (MATRIX S, unsigned *res_i, unsigned *res_j, double *res_sum) {
     unsigned i, j;
-    double curr, maxSquare = -DBL_MAX;
-    *sum = 0;
+    double curr, max_square = -1;
+    *res_sum = 0;
 
     for (i = 0; i < S.num_rows; i++)
         for (j = i+1; j < S.num_cols; j++) {
             curr = m_at(S, i, j);
-            curr *= curr;
-            *sum += curr;
-            if (curr > maxSquare) {
-                maxSquare = curr;
-                *resI = i;
-                *resJ = j;
+            curr = square(curr);
+            *res_sum += curr;
+            if (curr > max_square) {
+                max_square = curr;
+                *res_i = i;
+                *res_j = j;
             }
         }
 }
@@ -60,33 +61,31 @@ void pivot_inplace(MATRIX M, ROTATION P) {
     m_at(M, i, j) = m_at(M, j, i) = 0;
 }
 
-#define maxIter     100
-#define epsilon     0.000005
 EIGENVS eigenvalues_eigenvectors(MATRIX S) {
     const unsigned n = S.num_rows;
     unsigned iter = 0, i, j;
-    double prevOff, currOff;
+    double prev_largest_off, curr_largest_off;
     ROTATION P;
     EIGENVS res;
     res.matrix = identity_matrix(n);
     if (is_null(res.matrix)) return NULL_EIGENVS;
 
-    find_largest_offdiagonal_and_sum_upper(S, &i, &j, &currOff);
+    find_largest_offdiagonal_and_sum_upper(S, &i, &j, &curr_largest_off);
     do {
-        prevOff = currOff;
+        prev_largest_off = curr_largest_off;
         /* calculate P */
         {
-            double theta = (m_at(S, j, j)-m_at(S, i, i)) / (2*m_at(S, i, j));
-            short sign = theta >= 0 ? 1 : -1;
+            double theta = (m_at(S, j, j) - m_at(S, i, i)) / (2 * m_at(S, i, j));
+            short sign = (theta >= 0) - (theta < 0);
             double t = sign / (sign*theta + sqrt(square(theta) + 1));
             double c = 1 / sqrt(square(t) + 1);
-            double s = t*c;
+            double s = t * c;
             P.c = c; P.s = s; P.i = i; P.j = j;
         }
         pivot_inplace(S, P);
         left_rotate_inplace(res.matrix, P);
-        find_largest_offdiagonal_and_sum_upper(S, &i, &j, &currOff);
-    } while (++iter < maxIter && prevOff - currOff > epsilon);
+        find_largest_offdiagonal_and_sum_upper(S, &i, &j, &curr_largest_off);
+    } while (++iter < maxIter && prev_largest_off - curr_largest_off > epsilon);
 
     /* extract eigenvalues */
     {
