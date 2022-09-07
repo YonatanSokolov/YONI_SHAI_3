e82f2219688444a51395a0b7657420aaf63122e9
yonisokolov <sokolov1@mail.tau.ac.il>
2022-09-07 16:08:34 +0300

    anci-c for calculate_w

--- src/calculate_w.c
+++ src/calculate_w.c
@@ -1,15 +1,16 @@
 #include "datastructures.h"
 #include <math.h>
 
-// #include <stdio.h>
+/* #include <stdio.h>*/
 
-//step1 - create fun called "distanceL2"
-//step2 - use math.h to calculate exp(-...)
-//step3 - use shais matrix class and create the new W matrix
+/*step1 - create fun called "distanceL2"*/
+/*step2 - use math.h to calculate exp(-...)*/
+/*step3 - use shais matrix class and create the new W matrix*/
 
 double distance_L2(double *v1, double *v2, unsigned int len){
     double result = 0;
-    for (int a = 0; a < len ; a++ ){
+    int a;
+    for (a = 0; a < len ; a++ ){
         result += (v1[a] - v2[a])*(v1[a] - v2[a]);
     }
     result = sqrt(result);
@@ -18,15 +19,16 @@ double distance_L2(double *v1, double *v2, unsigned int len){
 
 MATRIX create_W(MATRIX V)
 {
+    int i ,j;
     unsigned n = V.num_cols;
     MATRIX w = alloc_matrix(n, n);
-    for(int i=0; i<n; i++){
-        for(int j=0;j<n;j++){
+    for(i=0; i<n; i++){
+        for(j=0;j<n;j++){
             if (i==j){
                 m_at(w,i,j) = 0;
             }
             else{
-                    // printf("exp res %lf\n", exp(-distance_L2(V->data[i],V->data[j],V->num_rows)/2));
+                    /* printf("exp res %lf\n", exp(-distance_L2(V->data[i],V->data[j],V->num_rows)/2));*/
                     m_at(w,i,j) = exp(-distance_L2(&m_at(V,i,0),&m_at(V,j,0),V.num_rows)/2);
             }
         }
