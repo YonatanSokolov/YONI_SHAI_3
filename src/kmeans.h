b4f6a0a87a112e82a39ec2c41a2b64c6b5730fd0
yonisokolov <sokolov1@mail.tau.ac.il>
2022-09-07 18:32:07 +0300

    wrapper update

--- src/kmeans.h
+++ src/kmeans.h
@@ -3,6 +3,12 @@
 
 #include "datastructures.h"
 
-int kmeans(MATRIX vectors, MATRIX initial_centrods);
+int kmeans(double         *vectors_array,
+                   double         *centroids_array,
+                   unsigned int  vectors_num,
+                   unsigned int  vector_dim);
+/*
+this is a wrapper function - from last project.
+*/
 
 #endif /*__kmeans__*/
